f3a058bf0af3177bc7f9ddee8dd57f0ddcfe92b3
petrlarionov <petrlarionov@192.168.1.97>
2023-12-17 17:22:01 +0200

    AppTest update

--- TestForTestNG/src/test/java/TestForTestNG/TestForTestNG/AppTest.java
+++ TestForTestNG/src/test/java/TestForTestNG/TestForTestNG/AppTest.java
@@ -1,33 +1,46 @@
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.testng.annotations.Test;
+package TestForTestNG.TestForTestNG;
 
-@Test
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.request.*;
+import org.springframework.test.web.servlet.result.*;
+
 @SpringBootTest
-@AutoConfigureMockMvc
-public class LoginControllerTest {
+public class AppTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @BeforeClass
+    public void setup() {
+       
+    	
+    	
+    }
+
     @Test
     public void testLoginPage() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/login-page"))
+       
+    	mockMvc.perform(MockMvcRequestBuilders.get("/login-page"))
                .andExpect(MockMvcResultMatchers.status().isOk())
                .andExpect(MockMvcResultMatchers.view().name("login"));
+   
     }
 
     @Test
     public void testLoginSuccess() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/login").param("login", "valid_login").param("password", "valid_password"))
+      
+    	mockMvc.perform(MockMvcRequestBuilders.post("/login").param("login", "valid_login").param("password", "valid_password"))
                .andExpect(MockMvcResultMatchers.status().isOk())
                .andExpect(MockMvcResultMatchers.view().name("login"))
                .andExpect(MockMvcResultMatchers.model().attribute("successMessage", true));
+   
     }
+    
 }
+
 
 
