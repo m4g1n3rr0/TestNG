f14581925c82509bea7e8a9882fd55ce3e1ac899
petrlarionov <petrlarionov@192.168.1.97>
2023-12-17 10:21:16 +0200

    Initial commit

---
+++ TestForTestNG/src/test/java/TestForTestNG/TestForTestNG/AppTest.java
@@ -0,0 +1,20 @@
+package TestForTestNG.TestForTestNG;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
